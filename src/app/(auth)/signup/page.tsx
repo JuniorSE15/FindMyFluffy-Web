cb262acf2b0949afe878b603a2445f8e6e4c548c
Bowornthat <boowornthat.maize@gmail.com>
2025-09-14 19:34:10 +0700

    feat: create welcome, signin, signup pages

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,21 @@
+import { Navigator } from '../components/navigation/navigator';
+import { SignUpForm } from '../components/form/signup-form';
+import { Footer } from '../components/footer/footer';
+
+export default function SignUp() {
+  return (
+    <div className='flex min-h-screen w-full flex-col items-center px-10 py-6'>
+      <Navigator currentPage='Sign Up' />
+      <div className='mt-20 flex flex-col gap-2 text-center'>
+        <h1 className='text-primary text-2xl font-semibold'>Welcome</h1>
+        <p className='text-secondary-text text-sm'>
+          Please enter your account here
+        </p>
+      </div>
+      <div className='mt-auto flex h-full w-full flex-col items-center justify-center gap-16'>
+        <SignUpForm />
+        <Footer type='signup' />
+      </div>
+    </div>
+  );
+}
