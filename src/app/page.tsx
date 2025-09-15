1239edaa9c8893ab53e18db7f5fbcc4f1a333619
Bowornthat <boowornthat.maize@gmail.com>
2025-09-15 10:51:08 +0700

    feat: add navigation bars to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+'use client';
+
+import { BottomNavigationBar } from '@/components/navigation/bottom-navigation-bar';
+import { TopNavigationBar } from '@/components/navigation/top-navigation-bar';
 import Image from 'next/image';
+import { useState } from 'react';
 
 export default function Home() {
+  const [activeTab, setActiveTab] = useState<'lost' | 'found'>('lost');
   return (
-    <div>
-      <h1>Find My Fluffy</h1>
+    <div className='relative flex min-h-screen flex-col items-center justify-center'>
+      <div className='sticky top-0 left-0 z-50 w-full'>
+        <TopNavigationBar activeTab={activeTab} setActiveTab={setActiveTab} />
+      </div>
+      <div className='flex-1'>
+        <h1>Find My Fluffy</h1>
+      </div>
+      <div className='sticky bottom-0 left-0 z-50 w-full'>
+        <BottomNavigationBar />
+      </div>
     </div>
   );
 }
