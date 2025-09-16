05cc0149f03f2acbf77302ecab45b5218aab44c1
Bowornthat <boowornthat.maize@gmail.com>
2025-09-16 21:50:33 +0700

    fix/refac: handle panning map view and the zoom and pin icons

--- src/app/(map)/components/map/map-component.tsx
+++ src/app/(map)/components/map/map-component.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { LatLng } from 'leaflet';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, ZoomControl } from 'react-leaflet';
 import { Button } from '@/components/ui/button';
 import { MapPinIcon } from 'lucide-react';
 import { NearbyPin } from './nearby-pin';
@@ -19,7 +19,7 @@ const DEFAULT_CENTER = new LatLng(13.736717, 100.523186);
 const TILE_LAYER_URL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const ATTRIBUTION =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-const DEFAULT_ZOOM = 15;
+const DEFAULT_ZOOM = 16;
 
 export default function MapComponent() {
   const [currentLocation, setCurrentLocation] = useState<LatLng | null>(null);
@@ -32,39 +32,43 @@ export default function MapComponent() {
     });
   }, []);
 
-  const locatCurrentLocation = () => {
+  const locateCurrentLocation = () => {
     setCurrentLocation(null);
     window.dispatchEvent(new CustomEvent('requestLocation'));
   };
 
   return (
-    <div className='relative h-full w-full'>
-      <div className='sticky top-0 left-0 z-50 w-full bg-white'>
+    <div className='relative h-full w-full max-w-xl overflow-hidden'>
+      <div className='absolute top-0 right-0 left-0 z-50 bg-white'>
         <TopBar />
       </div>
-      <MapContainer
-        center={currentLocation || DEFAULT_CENTER}
-        zoom={DEFAULT_ZOOM}
-        scrollWheelZoom={false}
-        className='z-0 h-full w-full'
-      >
-        <TileLayer attribution={ATTRIBUTION} url={TILE_LAYER_URL} />
-        <LocationMarker onLocationFound={setCurrentLocation} />
+      <div className='h-full w-full pt-16'>
+        <MapContainer
+          center={currentLocation || DEFAULT_CENTER}
+          zoom={DEFAULT_ZOOM}
+          scrollWheelZoom={false}
+          zoomControl={false}
+          className='z-0 h-full w-full'
+        >
+          <TileLayer attribution={ATTRIBUTION} url={TILE_LAYER_URL} />
+          <ZoomControl position='bottomright' />
+          <LocationMarker onLocationFound={setCurrentLocation} />
 
-        {NEARBY_PINS_OFFSETS.map((pinOffset) => {
-          const pin = createPinWithPosition(
-            pinOffset,
-            currentLocation || DEFAULT_CENTER,
-          );
-          return <NearbyPin key={pin.id} {...pin} />;
-        })}
-      </MapContainer>
+          {NEARBY_PINS_OFFSETS.map((pinOffset) => {
+            const pin = createPinWithPosition(
+              pinOffset,
+              currentLocation || DEFAULT_CENTER,
+            );
+            return <NearbyPin key={pin.id} {...pin} />;
+          })}
+        </MapContainer>
+      </div>
 
       <Button
         variant='outline'
         size='icon'
         className='absolute top-20 right-4 z-50'
-        onClick={locatCurrentLocation}
+        onClick={locateCurrentLocation}
       >
         <MapPinIcon size={24} className='text-primary' />
       </Button>
--- src/app/(map)/layout.tsx
+++ src/app/(map)/layout.tsx
@@ -2,7 +2,7 @@ import { BottomNavigationBar } from '@/components/navigation/bottom-navigation-b
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='relative flex h-screen flex-col overflow-hidden'>
+    <div className='relative flex h-screen flex-col'>
       <div className='flex-1 overflow-hidden'>{children}</div>
       <div className='sticky right-0 bottom-0 left-0 z-50 w-full'>
         <BottomNavigationBar />
